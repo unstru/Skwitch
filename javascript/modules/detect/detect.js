22dac5cb326254bd9656415c6c60d3c5ff9692f4
Cyril Agosta <cyril.agosta@gmail.com>
2013-01-15 01:14:08 +0100

    add a module for browser capability detection

---
+++ javascript/modules/detect/detect.js
@@ -0,0 +1,36 @@
+define('detect/detect',[
+	'Config/Config'
+], function(Config) {
+
+	var values = function() {
+		return {
+	    device : function() {
+	      return (this.get('isTablet')) ? "tablet" : this.get('isMobile') ? "mobile" : "desktop";
+	    },
+	    // These detections should not be here
+	    IEVersion : function() {
+	      var rv = -1; // Return value assumes failure.
+	      if (navigator.appName == 'Microsoft Internet Explorer') {
+	        var ua = navigator.userAgent,
+	        re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+	        if (re.exec(ua) != null)
+	          rv = parseFloat( RegExp.$1 );
+	      }
+	      return rv;
+	    },
+	    isIE : function() {
+	      return this.get('IEVersion') !== -1;
+	    },
+	    isMobile : (/iphone|ipod|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase())),
+	    isTablet : (/ipad|android 3|sch-i800|playbook|tablet|kindle|gt-p1000|sgh-t849|shw-m180s|a510|a511|a100|dell streak|silk/i.test(navigator.userAgent.toLowerCase())),
+	    isIpad : function() {
+	      return navigator.userAgent.match(/iPad/i) !== null;
+	    }
+	  };
+	};
+
+  return new Config({
+  	values : values()
+  });
+
+});
